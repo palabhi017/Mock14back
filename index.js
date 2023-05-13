97430bb77a4e251d778fd2d87ac60f5a61f9437f
palabhi017 <palabhi017@gmail.com>
2023-05-13 17:00:42 +0530

    mock14

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const app = express()
 const {connection} = require("./db")
+const mongoose = require("mongoose")
 const { userRouter } = require("./Routes/Auth.route")
 const { blogRouter } = require("./Routes/blog.route")
 const cors = require("cors")
 require("dotenv").config()
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.mongoURL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 app.use(cors())
 app.use(express.json())
 app.use("/user",userRouter)
@@ -12,12 +23,8 @@ app.use("/blogs",blogRouter)
 
 
 
-app.listen(process.env.port, async()=>{
-  try {
-    
-      await connection
-      console.log("port is running")
-  } catch (error) {
-    console.log(error)
-  }
+connectDB().then(() => {
+  app.listen(process.env.port, () => {
+      console.log("listening for requests");
+  })
 })
